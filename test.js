5478e146549fe85b23f9a0cdda91ed02fd6629ba
Austin Finn <astin21@gmail.com>
2017-08-03 20:25:51 +1000

    first commit for Nightmare project

---
+++ test.js
@@ -0,0 +1,44 @@
+// const Nightmare = require('nightmare')
+// const assert = require('assert')
+
+// describe('Load a Page', function() {
+//     this.timeout('30s')
+
+//     let nightmare = null
+//     beforeEach(() => {
+//         nightmare = new Nightmare()
+//     })
+
+// describe('/(Home Page)', () => {
+//     it('should load without error', done => {
+//         nightmare.goto('https://gethoodie.com')
+//             .end()
+//             .then(function(result) { done() })
+//             .catch(done)
+//         })
+//     })
+// })
+
+
+const Nightmare = require('nightmare')
+const assert = require('assert')
+
+describe('Load a Page', function() {
+  // Recommended: 5s locally, 10s to remote server, 30s from airplane ¯\_(ツ)_/¯
+  this.timeout('30s')
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
+  describe('/ (Home Page)', () => {
+    it('should load without error', done => {
+      // your actual testing urls will likely be `http://localhost:port/path`
+      nightmare.goto('https://rubygems.org')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
+  })
+})
